bc96cef458313ff1f19a8e8abc4458a7f1671922
Tigerball890 <littlelnu2@yahoo.com>
2016-04-05 20:55:09 -0400

    Added useful comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 exports.parse = parse = function(myString) {
+    // Check if myString is JSON
     if (myString.indexOf("{") === 0) {
         return JSON.parse(myString)
+        // Parse as JSON if it is
     }
     else {
         function primitiveToValue(primitive) {
@@ -20,42 +22,53 @@ exports.parse = parse = function(myString) {
             }
         }
         var fs = require("fs")
+        // Split document into array based on lines
         if (myString.indexOf("\n") > -1 && myString.indexOf("\r") === -1) {
             var lines = myString.split("\n")
         }
         else {
             var lines = myString.split("\r")
         }
+        // Start off by making the cheif object, obj, not undefined
         var obj = {}
         for (var i = 0; i < lines.length; i++) {
+            // If line has no arguments inside []
             if (!(lines[i].charAt(0) === "[")) {
                 var key = lines[i].split(", ")[0]
                 var value = lines[i].split(", ")[1]
                 if (value.indexOf("list: ") === 0) {
+                    // Get rid of list: , and , and ~s
                     value = value.replace("list: ", "").replace(/ and /g, ", ").replace(/\~([^< ](?:[^<]*?[^< ])?)\~/g, '[$1]')
+                    // Make array
                     value = JSON.parse(value)
                 }
                 obj[key] = value
             }
             else {
+                // Get line as without text inside [] and get bigKey as arguments inside []
                 var line = lines[i].replace(/\[.*?\]\s?/g, "")
                 var unBigKey = lines[i].replace(/.*?(\[[^)]*\]).*?/g, "")
+                // Can't use variable inside / /g
                 var myRegex = new RegExp(unBigKey, "g")
                 var bigKey = lines[i].replace(myRegex, "")
                 var key = line.split(", ")[0]
                 var value = line.split(", ")[1]
+                // Check if value should be array
                 if (value.indexOf("list: ") === 0) {
                     value = value.replace("list: ", "").replace(/ and /g, ", ").replace(/\~([^< ](?:[^<]*?[^< ])?)\~/g, '[$1]')
                     console.log(value)
                 }
                 bigKey = bigKey.replace(/\[/g, "").replace(/\]/g, "")
+                // If there is only one specification for category
                 if (bigKey.split(", ").length === 1) {
+                    // If only one argument is inside []
                     if (obj[bigKey]) {
                         var little = obj[bigKey]
                     }
                     else {
                         var little = {}
                     }
+                    // Check if value should be string
                     if (isNaN(value)) {
                         little[key] = value
                     }
@@ -65,21 +78,28 @@ exports.parse = parse = function(myString) {
                     obj[bigKey] = little
                 }
                 else {
+                    // For more arguments
                     string = "obj"
+                    // Loop through each argument and use eval to check if it is empty, if it is, make the value of it {}
                     for (var j = 0; j < bigKey.split(", ").length; j++) {
                         string += "." + bigKey.split(", ")[j]
                         if (!(eval(string)) || eval(string) === {}) {
                             eval(string + " = {}")
                         }
                     }
+                    // Assign the key a value
                     string += "." + key
+                    // If value is not string, write as raw value (Never going to be true, soon going to deprecate)
                     if (!(typeof value === 'string')) {
                         eval(string + " = " + value)
                     }
+                    // Here comes the real conditionals
                     else {
+                        // If value is string and is not equal to "true", "false", or is an integer in string form and is also not an array then treat it as a string in eval
                         if (!(value === "true" || value === "false") && isNaN(value) && value.indexOf("[") !== 0) {
                             eval(string + " = " + "\"" + value + "\"")
                         }
+                        // If value should not be string treat it as if it is not
                         else {
                             eval(string + " = " + value)
                         }
